7f075f847a5e0205abba0b94f40d155c8b55c402
eko-prog-code <ruthanggraeni24@gmail.com>
2021-04-28 10:23:37 +0700

    1st
    1st

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+   <div>
+      <h3 class="text-center pb-4">Godzilla vs. Kong is a 2021</h3>
+      <v-layout class="pb-12" align-center justify-center row wrap>
+      <video playsinline autoplay muted loop width="360px" height="340px" controls src="../assets/gvk.mp4">
+      <br>
+      <v-btn>play/stop</v-btn>
+      </video>
+      </v-layout>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
